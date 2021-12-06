c4bb5f4f3d603d9c5492eeb57418e6f3ea4da9de
oscaro <maller1234@hotmail.com>
2021-12-06 14:52:59 -0800

    +Add:  In tires creator gallery, delete navtext footer

---
+++ Themes/ImaginaTheme/views/pages/content/12.blade.php
@@ -0,0 +1,23 @@
+<div class="page page-{{$page->id}}>
+
+    <div class="page-content">
+<div class="container">
+    <x-media::single-image :isMedia="true" :mediaFiles="$page->mediaFiles()"  />
+    <div class="row">
+        <h1>{{$page->title}}</h1>
+    </div>
+
+    <div class="row">
+        <div class="page-body">
+            {!! $page->body !!}
+        </div>
+        <div class="col-md-12">
+            <x-media::gallery :mediaFiles="$page->mediaFiles()" :autoplay="false"
+                              :dots="false"
+                              :loop="true"
+                              :responsive="[300 => ['items' =>  1],700 => ['items' =>  2], 1024 => ['items' => 3]]"/>
+        </div>
+    </div>
+
+</div>
+</div>
