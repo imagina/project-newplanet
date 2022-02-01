0636345246ceae19f5b8716f1b2cfa025fbf738b
T-JULIAND <juliandavidbonillatorres@gmail.com>
2022-02-01 12:39:05 -0500

    Fix | Front-end : Pages

--- Themes/ImaginaTheme/views/pages/content/14.blade.php
+++ Themes/ImaginaTheme/views/pages/content/14.blade.php
@@ -1,28 +1,22 @@
 {{-- Page Contact --}}
-
 <div class="page page-{{ $page->id }}">
+  <div class="page-content">
+    <div class="container">
+      <div class="row">
+        <h1>{{ $page->title }}</h1>
+      </div>
 
-    <div class="page-content">
-        <div class="container">
-            
-            <div class="row">
-                <h1>{{ $page->title }}</h1>
-            </div>
+      <x-media::single-image :isMedia="true" :mediaFiles="$page->mediaFiles()"/>
 
-            <x-media::single-image :isMedia="true" :mediaFiles="$page->mediaFiles()" />
-
-            <div class="row">
-
-                <div class="page-body">
-                    {!! $page->body !!}
-                </div>
-
-                <div class="col-md-12">
-                    <x-iforms::form id="1" />
-                </div>
-
-            </div>
-
+      <div class="row">
+        <div class="page-body">
+          {!! $page->body !!}
         </div>
+
+        <div class="col-md-12">
+          <x-iforms::form id="3"/>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
--- Themes/ImaginaTheme/views/partials/slider-home.blade.php
+++ Themes/ImaginaTheme/views/partials/slider-home.blade.php
@@ -1,14 +1,13 @@
 <section id="SliderHome">
-    <div class="auto pb-5">
-    <x-slider::slider.owl 
-        id="1" 
-        :autoplayTimeout="3000" 
-        :margin="0" 
-        height="auto"
-        layout="slider-owl-layout-1"
-        dotsStyle="circle" 
-        :autoplay="true"
-        :autoplayTimeout="3000"
-        :loop="true"/> 
-    </div>
+  <div class="auto pb-5">
+    <x-slider::slider.owl
+            id="1"
+            :autoplayTimeout="3000"
+            :margin="0"
+            height="auto"
+            layout="slider-owl-layout-1"
+            dotsStyle="circle"
+            :autoplay="true"
+            :loop="true"/>
+  </div>
 </section>
