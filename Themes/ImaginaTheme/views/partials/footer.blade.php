3431a00865a03da9579fd6ee3a4f95c8b24e391a
T-JULIAND <juliandavidbonillatorres@gmail.com>
2022-02-01 12:38:18 -0500

    Fix | Front-end : restructure

--- Themes/ImaginaTheme/views/partials/footer.blade.php
+++ Themes/ImaginaTheme/views/partials/footer.blade.php
@@ -2,60 +2,58 @@
 
 <footer>
 
-    <div class="container-fluid">
-        <div class="container">
+  <div class="container-fluid">
+    <div class="container">
 
-            <div class="row data">
-                <div class="col-sm-4">
-                    <x-isite::logo name="logo1" imgClasses="header-logo-1" />
-                </div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-3">
-                    <div class="row">
-                        <x-isite::contact.addresses />
-                    </div>
-                    <div class="row">
-                        <x-isite::contact.phones />
-                    </div>
-                    <div class="row">
-                        <x-isite::contact.emails />
-                    </div>
-                </div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-3">
-                    <div class="row">
-                        <x-isite::contact.addresses :addresses="json_decode(setting('icustom::addresses',null,'[]'))" />
-                    </div>
-                    <div class="row">
-                        <x-isite::contact.phones :phones="json_decode(setting('icustom::phones',null,'[]'))" />
-                    </div>
-                    <div class="row">
-                        <x-isite::contact.emails :emails="json_decode(setting('icustom::emails',null,'[]'))" />
-                    </div>
-                </div>
-            </div>
-
-            <hr class="solid">
-
-            <div class="row">
-                <div class="col-sm-12">
-                    <x-isite::menu id="mainMenu" :withHome="false" layout="category-menu-layout-2" menuBefore="main" />
-                </div>
-            </div>
-            
-            <hr class="solid">
-
-            <div class="row bottom-footer">
-                <div class="col-sm-8">
-                    @include('partials.logoimagina')
-                    <p>{{ trans('icustom::common.copyrightfooter') }}</p>
-                </div>
-                <div class="col-6 col-sm-4 social-contact">
-                    <x-isite::social />
-                </div>
-            </div>
-
+      <div class="row data">
+        <div class="col-12 col-lg-4 d-flex justify-content-center">
+          <x-isite::logo name="logo1" imgClasses="header-logo-1"/>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="row">
+            <x-isite::contact.addresses/>
+          </div>
+          <div class="row">
+            <x-isite::contact.phones/>
+          </div>
+          <div class="row">
+            <x-isite::contact.emails/>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="row">
+            <x-isite::contact.addresses :addresses="json_decode(setting('icustom::addresses',null,'[]'))"/>
+          </div>
+          <div class="row">
+            <x-isite::contact.phones :phones="json_decode(setting('icustom::phones',null,'[]'))"/>
+          </div>
+          <div class="row">
+            <x-isite::contact.emails :emails="json_decode(setting('icustom::emails',null,'[]'))"/>
+          </div>
+        </div>
       </div>
+
+      <hr class="solid">
+
+      <div class="row">
+        <div class="col-sm-12">
+          <x-isite::menu id="mainMenu" :withHome="false" layout="category-menu-layout-2" menuBefore="main"/>
+        </div>
+      </div>
+
+      <hr class="solid">
+
+      <div class="row bottom-footer">
+        <div class="col-sm-8">
+          @include('partials.logoimagina')
+          <p>{{ trans('icustom::common.copyrightfooter') }}</p>
+        </div>
+        <div class="col-6 col-sm-4 social-contact">
+          <x-isite::social/>
+        </div>
+      </div>
+
     </div>
+  </div>
 
 </footer>
